1915867b3e841c5f3ec0693ab91babc31b33549e
GabrielDieck <gabrieldieck16@gmail.com>
2022-09-28 23:42:11 -0400

    Revert "."
    This reverts commit 73992c8af6f7f0e1cb74b838abcf64c0a1c06d41.

--- Clientes/AgregarCliente.Designer.cs
+++ Clientes/AgregarCliente.Designer.cs
@@ -159,7 +159,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(280, 95);
+            this.button1.Location = new System.Drawing.Point(345, 96);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 12;
