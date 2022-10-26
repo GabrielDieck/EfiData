f254204cd2f57df7387a91e620262acd5539c043
GabrielDieck <gabrieldieck16@gmail.com>
2022-10-26 20:33:07 -0300

    .

--- Clientes/!Busqueda.Designer.cs
+++ Clientes/!Busqueda.Designer.cs
@@ -75,7 +75,6 @@ namespace Clientes
             this.label2.Size = new System.Drawing.Size(111, 21);
             this.label2.TabIndex = 1;
             this.label2.Text = "Razón Social:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
